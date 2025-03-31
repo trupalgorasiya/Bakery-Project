b3369fcb829460331ae099bd7befcd02da4721bc
Trupal Gorasiya <trupalgorasiya510@gmail.com>
2025-03-31 09:24:46 +0530

    Update settings.py

--- novabrakers/sweetdelights/sweetdelights/settings.py
+++ novabrakers/sweetdelights/sweetdelights/settings.py
@@ -30,8 +30,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "trupalgorasiya510@gmail.com"  # Replace with your email
-EMAIL_HOST_PASSWORD = "vtoo ydzb gsnh houc"  # Use App Password, not your email password
+EMAIL_HOST_USER = "abc@gmail.com"  # Replace with your email
+EMAIL_HOST_PASSWORD = "**** **** **** ****"  # Use App Password, not your email password
 
 
 # Application definition
